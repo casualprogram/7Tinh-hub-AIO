aa3c59f72ec5815da78dd10e1921dd11d3b2f6c0
casualprogram <datdatdat2k@gmail.com>
2025-02-25 22:47:17 -0500

    adjust push-news so it can push out all the news

--- src/interfaces/push_news.js
+++ src/interfaces/push_news.js
@@ -1,15 +1,17 @@
 import getFirstData from "../modules/fetch/fetch_source_1.js";
-import formattedFirstData from "../modules/helper/formatting_data.js";
+import formattedData from "../modules/helper/formatting_data.js";
 import { resolve } from 'path';
 import path from 'path';
 import dotenv from 'dotenv';
+import getSecondData from '../modules/fetch/fetch_source_2.js';
 
 
 dotenv.config({ path: resolve('../../.env') });
 
 const SOURCE_URL_1 = process.env.SOURCE_1;
 
-const filePath = path.resolve('../data/source1/stories.json');
+const filePath1 = path.resolve('../data/source1/stories.json');
+const filePath2 = path.resolve('../data/source1/stories2.json');
 
 
 /**
@@ -18,11 +20,17 @@ const filePath = path.resolve('../data/source1/stories.json');
  */
 export default async function push_news(){
     // Fetch the data from the source
-    await getFirstData(filePath, SOURCE_URL_1);
-    console.log("\n----------\n ---> SUCCESSFULLY FETCH COMPLEX DATA\n----------");
+    await getFirstData(filePath1, SOURCE_URL_1);
+    console.log("\n----------\n ---> SUCCESSFULLY FETCH FIRST DATA\n----------");
+
+    console.log("\n----------\n ---> SUCCESSFULLY FETCH SECOND DATA\n----------");
+    await getSecondData();
+
 
     // Format the data and send it to the community
-    await formattedFirstData(filePath);
+    await formattedData(filePath1);
+    await formattedData(filePath2)
+
     console.log("\n----------\n --> SEND TO THE COMMUNITY...\n----------");
     
 }
